af8fb3a337f4bcdb2bfe61f3484c195302c052fd
OilPainter1 <hjcryns@gmail.com>
2022-10-27 14:01:46 -0500

    added first question and imported inquirer

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,24 @@
 // TODO: Include packages needed for this application
+import inquirer from "inquirer";
+import generateMarkdown from "./utils/generateMarkdown.js";
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: "input",
+    name: "projectName",
+    message: "What is the name of the project?"
+},
+];
+inquirer.prompt(questions).then((answers)=> console.log(answers))
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    console.log("test")
+}
 
 // Function call to initialize app
 init();
