59dddff4a077240728522a9c28da55bd6da1e979
OilPainter1 <hjcryns@gmail.com>
2022-11-02 15:00:30 -0500

    debugged internal links in table of contents

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -18,7 +18,28 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  var returnLink = ''
+  var index = 1
+  if (license.includes('ISC')){
+    returnLink += index +". [ISC](https://opensource.org/licenses/ISC)\n"
+    index++
+    
+  } 
+   if (license.includes('GNU GPLv3')){
+    returnLink += index +". [GNU GPLv3](https://opensource.org/licenses/GPL-3.0)\n"
+    index++
+  }  
+  if (license.includes('MIT')){
+    returnLink += index + ". [MIT](https://opensource.org/licenses/MIT)\n"
+    index++
+  } 
+  if (license.includes('Mozilla Public License 2.0')){
+    returnLink += index + ". [Mozilla Public License 2.0](https://www.mozilla.org/en-US/MPL/2.0/)\n"
+    index++
+    }
+  return returnLink
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -30,21 +51,19 @@ function generateMarkdown(data) {
   ## Description
   ${data.description}
   ## Table of Contents
-    1. [Installation](#Installation)
-    2. [Usage](#Usage)
-    3. [Licensing](#Licensing)
-    4. [Questions](#Questions)
+  1. [Installation](#installation)
+  2. [Usage](#usage)
+  3. [Licensing](#licensing)
+  4. [Questions](#questions)
   ## Installation
   ${data.installation}
   ## Usage
   ${data.usage}
   ## Licensing
-  ${data.license}
-  //include licensing methods
-
+  ${renderLicenseLink(data.license)}
   ## Questions
-  email: ${data.email}
-  github: ${data.github}
+  - email: ${data.email}
+  - github: ${data.github}
 `;
 }
 
